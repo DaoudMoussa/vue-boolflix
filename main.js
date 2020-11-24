e20c41d2f7ca3accb3f535a332505851b9fdea8c
Daoud <99m.daoud@gmail.com>
2020-11-24 16:22:07 +0100

    Aggiunta stampa lista film

--- main.js
+++ main.js
@@ -11,12 +11,13 @@ const app = new Vue({
             const params = {
                 api_key: 'a6029345db17cec20afdcb6beac01172',
                 query: this.searchedText,
-                language: it
+                language: "it"
             }
             axios.get('https://api.themoviedb.org/3/search/movie', {params})
                 .then(result => {
                     this.foundMovies = result.data.results;
                 });
+            this.searchedText = '';
         }
     }
 });
