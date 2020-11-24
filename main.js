f3bf98da07824ba24220afab021e0a2a4451b65d
Daoud <99m.daoud@gmail.com>
2020-11-24 18:53:25 +0100

    Aggiunta stampa serie tv

--- main.js
+++ main.js
@@ -2,31 +2,45 @@
 const app = new Vue({
     el: '#root',
     data: {
-        foundMovies: [],
-        foundTVShows: [],
+        movies: {
+            foundMovies: [],
+            visible: false
+        },
+        tvShows: {
+            foundTVShows: [],
+            visible: false
+        },
         searchedText: '',
         availableFlags: ['en', 'it', 'es', 'fr', 'zh']
     },
     methods: {
         search() {
-            let params = {
+            const params = {
                 api_key: 'a6029345db17cec20afdcb6beac01172',
                 query: this.searchedText,
                 language: "it"
             }
+
             axios.get('https://api.themoviedb.org/3/search/movie', {params})
                 .then(result => {
-                    this.foundMovies = result.data.results;
+                    this.movies.foundMovies = result.data.results;
                 });
-            this.searchedText = '';
 
-            // axios.get('https://api.themoviedb.org/3/search/tv?api_key=a6029345db17cec20afdcb6beac01172&query=la casa&language=it', {})
-        },
-        errore() {
-                console.log('errore');
+            axios.get('https://api.themoviedb.org/3/search/tv', {params})
+                .then(result => {
+                    this.tvShows.foundTVShows = result.data.results;
+                })
+
+            this.searchedText = '';
         },
         stars(rating) {
             return Math.round(rating / 2);
+        },
+        toggleTVShows() {
+            this.tvShows.visible = !this.tvShows.visible;
+        },
+        toggleMovies() {
+            this.movies.visible = !this.movies.visible;
         }
     }
 });
