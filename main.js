de66561e017376f666e9a7579f79e1422623b22a
Daoud <99m.daoud@gmail.com>
2020-11-24 16:01:29 +0100

    aggiunta chiamata API al click del button e all'invio

--- main.js
+++ main.js
@@ -2,10 +2,21 @@
 const app = new Vue({
     el: '#root',
     data: {
+        foundMovies: [],
+        searchedText: '',
+
     },
     methods: {
-    },
-    mounted() {
-
+        search() {
+            const params = {
+                api_key: 'a6029345db17cec20afdcb6beac01172',
+                query: this.searchedText,
+                language: it
+            }
+            axios.get('https://api.themoviedb.org/3/search/movie', {params})
+                .then(result => {
+                    this.foundMovies = result.data.results;
+                });
+        }
     }
 });
